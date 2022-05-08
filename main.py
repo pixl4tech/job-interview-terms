f01f2bb81ae0f34e5a7da42c8f1da89bedd98cc6
Dmitry Kolosov <52660454+pixl4tech@users.noreply.github.com>
2022-05-08 21:18:46 +0400

    add freq words

--- main.py
+++ main.py
@@ -4,17 +4,30 @@ import nltk
 from nltk.corpus import stopwords
 from nltk.probability import FreqDist
 import re
+import csv
 
 
 raw_text = load(open("raw_text.pkl", 'rb'))
-raw_text = re.sub(r'[^\w\s]+', '', raw_text)
+raw_text = re.sub(r'[^\w\s]+', '', raw_text).lower()
 
 text_tokens = nltk.word_tokenize(raw_text)
 stopwords = stopwords.words("english")
 
+freq_words = []
+with open('freq_words.csv') as File:
+    reader = csv.reader(File, delimiter=';')
+    for row in reader:
+        freq_words.append(row[1].strip())
+
+usual_words = list(map(lambda x: x.replace("'", ""), list(set(freq_words[:150] + stopwords))))
+
+print(usual_words)
+
+
+
 text_tokens_flt = []
 for w in text_tokens:
-    if w not in stopwords:
+    if w not in usual_words:
         text_tokens_flt.append(w)
 
 text = nltk.Text(text_tokens_flt)
